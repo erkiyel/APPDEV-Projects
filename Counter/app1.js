51c5980cded88f92db8eda7083945fe2144cf188
olores_e <olores_e@f112.lan>
2024-08-28 12:26:11 +0800

    counter project

---
+++ Counter/app1.js
@@ -0,0 +1,39 @@
+//
+let count = 0
+//
+const btns = document.querySelectorAll('.btn')
+const value = document.querySelector('#value')
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.currentTarget.classList)
+        const styles = e.currentTarget.classList
+
+        if(styles.contains('decrease')){
+            count--
+        }else if(styles.contains('increase')){
+            count++
+        }else if(styles.contains('inc')){
+            count += 5
+        }else if(styles.contains('dec')){
+            count -= 5
+        }else if(styles.contains('random')){
+            count = Math.floor(Math.random() * 100)
+        }else{
+            count = 0
+        }
+
+        if(count > 0){
+            value.style.color="red"
+        }else if(count < 0){
+            value.style.color="green"
+        }else{
+            value.style.color="black"
+        }
+        value.textContent = count
+    })
+})
+
+getRandomNumber = () => {
+    return Math.floor(Math.random() * 100)
+}
