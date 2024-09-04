e2e7e4f76e8de740a1397a0d765f368a99221366
olores_e <olores_e@f112.lan>
2024-09-04 13:40:57 +0800

    react and nav bar activity

---
+++ my-first-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+//this is my footer component
+export default function Footer() {
+    return(
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} My Food Website || Written by: Tom Hanks
+            </p>
+        </footer>
+    )
+}
